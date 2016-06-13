e537c1338b1889c1d33c6ec2e9892ed0265ac42c
Haibin Wang <haibin.wang@rea-group.com>
2016-06-13 19:15:43 +1000

    Add entrance for different platforms

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,21 @@
 
 'use strict';
 
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import {
   AppRegistry,
+  Platform,
+  View,
 } from 'react-native';
-
 var TabBarViewIOS = require("./js/Tab/TabBarView").default
 
 class TechRadar extends Component {
   render() {
-    return (
-        <TabBarViewIOS/>
-    );
+    if (Platform.OS === 'ios') {
+      return <TabBarViewIOS/>
+    } else {
+      return <View/>
+    }
   }
 }
 
