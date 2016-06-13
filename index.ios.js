21d4f8e17870fbb1d6cc7d5c63e86f0b02a7e6aa
Haibin Wang <haibin.wang@rea-group.com>
2016-06-13 19:28:43 +1000

    Revert "Add entrance for different platforms"
    This reverts commit e537c1338b1889c1d33c6ec2e9892ed0265ac42c.

--- index.ios.js
+++ index.ios.js
@@ -1,21 +1,18 @@
 
 'use strict';
 
-import React, { Component, } from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
-  Platform,
-  View,
 } from 'react-native';
+
 var TabBarViewIOS = require("./js/Tab/TabBarView").default
 
 class TechRadar extends Component {
   render() {
-    if (Platform.OS === 'ios') {
-      return <TabBarViewIOS/>
-    } else {
-      return <View/>
-    }
+    return (
+        <TabBarViewIOS/>
+    );
   }
 }
 
